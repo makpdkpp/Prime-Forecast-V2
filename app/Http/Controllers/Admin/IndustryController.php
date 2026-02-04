acb4c129eb0c72dd39e44db219ac1bff8e790f26
makpdkpp <b_kongman@hotmail.com>
2026-02-04 20:24:45 +0700

    Update IndustryController.php

--- app/Http/Controllers/Admin/IndustryController.php
+++ app/Http/Controllers/Admin/IndustryController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Illuminate\Support\Facades\DB;
+
 class IndustryController extends MasterDataController
 {
     protected $table = 'industry_group';
@@ -10,4 +12,20 @@ class IndustryController extends MasterDataController
     protected $viewPath = 'admin.master.generic';
     protected $routeName = 'admin.industries';
     protected $title = 'อุตสาหกรรม';
+
+    public function destroy($id)
+    {
+        $usageCount = DB::table('company_catalog')
+            ->where('Industry_id', $id)
+            ->count();
+
+        if ($usageCount > 0) {
+            return redirect()
+                ->route("{$this->routeName}.index")
+                ->with('error', "ไม่สามารถลบ{$this->title}นี้ได้ เนื่องจากมีบริษัทที่ใช้งานอยู่ {$usageCount} รายการ");
+        }
+
+        DB::table($this->table)->where($this->primaryKey, $id)->delete();
+        return redirect()->route("{$this->routeName}.index")->with('success', "ลบ{$this->title}เรียบร้อยแล้ว");
+    }
 }
